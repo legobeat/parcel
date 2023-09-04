660f318252b885924ae0d07acad54d7a5d2ec1e8
legobt <6wbvkn0j@anonaddy.me>
2023-09-04 06:23:02 +0000

    feat(transformers/mdx): add compileOpts parameter

--- packages/transformers/mdx/src/MDXTransformer.js
+++ packages/transformers/mdx/src/MDXTransformer.js
@@ -1,13 +1,14 @@
 // @flow
 import {Transformer} from '@parcel/plugin';
+import {CompileOptions} from '@mdx-js/mdx';
 
 export default (new Transformer({
-  async transform({asset}) {
+  async transform({asset}, compileOpts?: CompileOptions) {
     let {compile} = await import('@mdx-js/mdx');
     let code = await asset.getCode();
 
     try {
-      let compiled = await compile(code);
+      let compiled = await compile(code, compileOpts);
       asset.type = 'js';
       asset.setCode(compiled.value);
     } catch (e) {
